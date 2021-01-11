b5d0c15e330df4465bca1b360565b621d56767a1
HunterMV <67179315+HunterMV@users.noreply.github.com>
2021-01-11 10:52:23 -0800

    Sound Testing

--- Assets/Scripts/GridSpace.cs
+++ Assets/Scripts/GridSpace.cs
@@ -7,8 +7,14 @@ public class GridSpace : MonoBehaviour
 {
     public Button button;
     public Text buttonText;
+    public AudioSource placeDown;
 
     private GameController gameController;
+
+    public void Start()
+    {
+        placeDown = GetComponent<AudioSource>();
+    }
 
     public void SetGameControllerReference (GameController controller)
     {
@@ -18,6 +24,7 @@ public class GridSpace : MonoBehaviour
     public void SetSpace()
     {
         buttonText.text = gameController.setPlayerSide();
+       placeDown.Play(0);
         button.interactable = false;
         gameController.EndTurn();
     }
